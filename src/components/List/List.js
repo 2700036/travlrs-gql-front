b7c6063d9c202c45ccf83a2793a28060786480c5
Sergey Biryukov <2700036@mail.ru>
2021-02-22 14:00:46 +0300

    upd

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { useRouteMatch, useHistory, useLocation } from 'react-router-dom';
+import { useRouteMatch, useHistory } from 'react-router-dom';
 import ImagePopup from '../ImagePopup/ImagePopup';
 import Spinner from '../Spinner/Spinner';
 
 export default function List({ items, itemComponent }) {
   const match = useRouteMatch();
   const history = useHistory();
-  const location = useLocation();
-  const photoId = match.params.id;
+  const path = history.location.pathname.match(/\/[a-z\/]+/)[0]
+  let photoId = match.params.id;
   const makeListOfElements = (elements, Component) => {
     return elements.map((card) => <Component card={card} key={card._id} />);
   };
-  console.log(items)
-
+  
   return <>
   <ul className='places__list'>
     {makeListOfElements(items, itemComponent)}
     </ul>
-    {photoId && (<ImagePopup card={items.find(({ _id }) => photoId === _id)} onClose={() => history.push('/cards/')} /> || <Spinner/>)}
+    {photoId && (<ImagePopup card={items.find(({ _id }) => photoId === _id)} onClose={() => history.push(path)} /> || <Spinner/>)}
     
     </>;
 }
